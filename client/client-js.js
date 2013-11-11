1fdab115bfb17e04bb71cf33b9d98d2e7fbd6545
Sajit Mathew Kunnumkal <skunnumkal@amplify.com>
2013-11-10 23:25:41 -0500

    Client draft 1

---
+++ client/client-js.js
@@ -0,0 +1,35 @@
+var ARatingService = ARatingService || {};
+
+ARatingService.WebServiceConnector = function WebServiceConnector(subKey, uuid) {
+    'use strict';
+   var host= "localhost:9000";
+   var buildRatingsRequest = function(productId){
+       return host+"/product/"+productId+"/ratings";
+   };
+   var makeCall = function(url,type,payload){
+       var xmlHttp = null;
+
+       xmlHttp = new XMLHttpRequest();
+       xmlHttp.open( type, url, false );
+       xmlHttp.send( payload );
+       return xmlHttp.responseText;
+   };
+   this.getRatings = function(productId){
+        var url = buildRatingsRequest(productId);
+        var response = makeCall(url,"GET",null);
+        console.log(response);
+        //make a call to backend  use XmlHttpRequest
+   };
+   this.addRating = function(productId,rating,who,review){
+        var url = buildRatingsRequest(productId);
+        var ratingObj = {
+            'sourceId' : who,
+            'ratingMap' : rating,
+            'review' : review
+        };
+        var response = makeCall(url,"POST",ratingObj);
+       console.log(response);
+   };
+
+
+};
