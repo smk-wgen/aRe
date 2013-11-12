49f3562ba6174ac93ea63df7614536dd5e5300ff
Sajit Mathew Kunnumkal <skunnumkal@amplify.com>
2013-11-12 07:10:13 -0500

    Adding CORS

---
+++ reweb/app/controllers/CorsAction.scala
@@ -0,0 +1,38 @@
+package controllers
+import play.api._
+import play.api.mvc._
+
+
+object CORSAction {
+
+  type ResultWithHeaders = Result { def withHeaders(headers: (String, String)*): Result }
+
+  def apply(block: Request[AnyContent] => ResultWithHeaders): Action[AnyContent] = {
+    Action { request =>
+      block(request).withHeaders("Access-Control-Allow-Origin" -> "*")
+    }
+  }
+
+  def apply(block: => ResultWithHeaders): Action[AnyContent] = {
+    println("In apply method")
+    this.apply(_ => block)
+  }
+ 
+  def foo[AnyContent](bp:BodyParser[AnyContent])(block:Request[AnyContent] => ResultWithHeaders):Action[AnyContent] = {
+    Action(bp) { request =>
+      block(request).withHeaders("Access-Control-Allow-Origin" -> "*")
+    }
+  }
+ 
+
+}
+trait CORS{
+  def MCORSAction[A](bp: BodyParser[A])(f: Request[A] => Result): Action[A] = {
+	  Action(bp) { request =>
+	    Logger.info("Calling action")
+	    f(request)
+	  }
+  }
+  
+ 
+}
